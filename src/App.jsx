eeb3e1d1d4eb655f04d61521da6576f2e057ee94
EmiTorres93 <emilianat1992@gmail.com>
2023-07-10 21:26:49 -0300

    Add React-Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import Titulo from "./components/Titulo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <>
+    <Container>
       <Titulo></Titulo>
-    </>
+      {/*<Títulos>*/}
+    </Container>
   );
 }
 
--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -2,11 +2,12 @@ const Titulo = () => {
   //Aquí vamos a cargar toda la lógica
   return (
     //aqui tb puedo agregar lógica
-    <>
+    <section className="text-center">
       {/* aquí va todo el maquetado*/}
-      <h1>Proyecto Contador</h1>
-      <h2>Este es mi subtítulo</h2>
-    </>
+      <h1 className="display-4">Proyecto Contador</h1>
+      <h2 className="display-6">Este es mi subtítulo</h2>
+      <hr></hr>
+    </section>
   );
 };
 
